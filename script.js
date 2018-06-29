94927aeb5c3769b4c865c2adb6452a8520d22cfd
ioni_dev <ioni_dev@protonmail.com>
2018-06-28 23:09:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById("button-div").addEventListener("click", function (e){
+  let target = e.target;
+  let gal1 = document.getElementById("ga");
+  let gal2 = document.getElementById("gallery2");
+  let gal3 = document.getElementById("gallery3");
+  
+
+  if(target.id == '1'){
+      if( gal1.className == "z"){
+        gal3.classList.add("hidden");
+        gal1.className = "z animated fadeIn";
+        
+      }else{
+        gal1.className = "z animated fadeIn";
+        
+        gal2.classList.add("hidden");
+        gal3.classList.add("hidden");
+      } 
+  }else if(target.id == '2'){
+    if(gal2.className == "z"){
+      gal1.classList.remove("z");
+      gal1.classList.add("gallery");
+      
+      
+    }else{
+      gal2.className = "z animated fadeIn";
+      gal3.classList.add("hidden");
+    }  
+  }else if(target.id =='3'){
+    if(gal3.className == "z"){
+      gal1.classList.remove("z");
+      gal2.classList.remove("z");
+      
+    }else{
+      gal3.className = "z animated fadeIn";
+    }
+  }
+});
